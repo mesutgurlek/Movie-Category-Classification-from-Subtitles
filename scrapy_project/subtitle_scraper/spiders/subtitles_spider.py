c1b54711df29aefa318cce8cdf74e55cf2398867
alierdogan7 <you@example.com>
2016-11-17 01:20:26 +0300

    code revisited- works like charm (Insh'a Allah)

--- scrapy_project/subtitle_scraper/spiders/subtitles_spider.py
+++ scrapy_project/subtitle_scraper/spiders/subtitles_spider.py
@@ -4,9 +4,10 @@ import scrapy
 import os
 import xmlrpc.client as xrpc
 
-subtitles_path = "/Users/mesutgurlek/Documents/Machine Learning/project/Movie-Category-Classification-from-Subtitles/Subtitles"
+# subtitles_path = "/Users/mesutgurlek/Documents/Machine Learning/project/Movie-Category-Classification-from-Subtitles/Subtitles"
+subtitles_path = "/home/burak/Documents/Courses-2016f/CS464/Project/Subtitles"
 url_template = "http://www.imdb.com/search/title?genres=%s&explore=genres&sort=num_votes,desc&view=simple"
-imdb_page_limit = 1
+imdb_page_limit = 5
 
 server = xrpc.ServerProxy("http://api.opensubtitles.org/xml-rpc")
 token = server.LogIn("randomwalker", "sub1machine", "en", "MachineTitle").get("token")
@@ -72,11 +73,15 @@ class SubtitlesSpider(scrapy.Spider):
         subtitles = {} # key => IDSubtitleFile, value => Metadata of subtitle
 
         # USING IMDB ID'S, DOWNLOAD THEIR METADATA AND SELECT ID OF A SUITABLE SUBTITLE FOR EACH MOVIE
-        for imdb_id in imdb_ids[:subtitle_per_category]:
+        # for imdb_id in imdb_ids[:subtitle_per_category]:
+        remaining = subtitle_per_category
+        i = 0
+        while remaining > 0 and i < len(imdb_ids):
+            imdb_id = imdb_ids[i]
             print("Searching subtitle for movie with ID: %s" % imdb_id)
             found_subtitles = server.SearchSubtitles(token, [{'imdbid': imdb_id, 'sublanguageid': 'eng'}])['data']
 
-            print("!!!!! Found subtitles: %s" % found_subtitles)
+            print("!!!!! Found %d subtitles for movie with ID: %s " % (len(found_subtitles), imdb_id))
 
             if impaired_support:
                 impaired_subtitles = list(filter(lambda sub: sub['SubHearingImpaired'] == '1', found_subtitles))
@@ -90,10 +95,14 @@ class SubtitlesSpider(scrapy.Spider):
 
             filename = "%s/%s/%s %s" % (subtitles_path, category_name, subtitle['MovieName'], impaired_label)
 
-            subtitles[subtitle['IDSubtitleFile']] = {'imdb_id': imdb_id, 'filename': filename,
+            # if this subtitle has already been downloaded before don't append it to array of subtitles to be downloadec
+            if not os.path.isfile(filename):
+                subtitles[subtitle['IDSubtitleFile']] = {'imdb_id': imdb_id, 'filename': filename,
                                                      'movie_name': subtitle['MovieName'],
                                                     'SubDownloadLink': subtitle['SubDownloadLink']}
+                remaining -= 1
 
+            i += 1
             # with open(filename, 'w') as f:
             #     f.write(json.dumps(subtitle['SubDownloadLink']))
             #     # yield parse_movie(imdb_id)
@@ -101,7 +110,7 @@ class SubtitlesSpider(scrapy.Spider):
 
         #DOWNLOAD SUBTITLES AND WRITE THEM INTO FILES
         print("Downloading subtitles of %s" % category_name)
-        subtitle_ids = [ idsubtitlefile for idsubtitlefile, val in subtitles.items()]
+        subtitle_ids = [ idsubtitlefile for idsubtitlefile, sub in subtitles.items()]
         subtitle_files_response = server.DownloadSubtitles(token, subtitle_ids)
 
         import base64
@@ -112,9 +121,13 @@ class SubtitlesSpider(scrapy.Spider):
             for subtitle_object in subtitle_files_response['data']: #each subtitle_object has base64 data and idsubtitlefile key
                 sub = subtitles[subtitle_object['idsubtitlefile']]
 
-                with open(sub['filename'], 'w') as file:
-                    file.write(gzip.decompress(base64.b64decode(subtitle_object['data'])).decode())
+                # I SOLVED THE ISSUE OPENING THE FILE IN BYTE-WRITING MODE AND DIRECTLY WRITING BYTES OBJECT TO FILE
+                # WITHOUT TRYING TO DECODE THE BYTES INTO A STRING
+                with open(sub['filename'], 'wb') as file:
+                    file.write(gzip.decompress(base64.b64decode(subtitle_object['data']))) #.decode())
                     print("Subtitle file saved into: %s" % sub['filename'])
+        else:
+            print("Subtitles cannot be downloaded! Status code: %s" % subtitle_files_response['status'])
 
     def parse_movie(self):
         pass
