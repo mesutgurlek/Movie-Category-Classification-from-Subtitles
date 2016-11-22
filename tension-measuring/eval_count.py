f314fe1e77298befe3c095bdfe7f7d44c3ba3052
alierdogan7 <you@example.com>
2016-11-22 14:06:53 +0300

    subtitle 'at_seconds' prop added, percentage_count method added

--- tension-measuring/eval_count.py
+++ tension-measuring/eval_count.py
@@ -15,6 +15,26 @@ def str_to_timedelta(str_tm="02:14:53,085"):
     return timedelta(hours=int(hours), minutes=int(mins), seconds=int(secs), milliseconds=int(millis))
 
 
+#gives the percentage dialog count array
+def count_percentage(subs):
+    end_of_movie = subs[-1].end
+    movie_length_mins = int(str_to_timedelta(end_of_movie).total_seconds() / 60 ) + 1
+    second_interval = (movie_length_mins + 1) * 60 / 100.0
+    counts = [0] * 100 #this will give the percentage dialog count array
+
+    secs = second_interval
+    index = 0
+    for sub in subs:
+        if sub.at_seconds < secs:
+            counts[index] += 1
+        else:
+            secs += second_interval
+            index += 1
+            counts[index] += 1
+
+    return counts if len(counts) == 100 else None
+
+
 def count_intervals(subs, minute_interval):
         end_of_movie = subs[-1].end
         movie_length_mins = int(str_to_timedelta(end_of_movie).total_seconds() / 60 ) + 1
@@ -32,15 +52,12 @@ def count_intervals(subs, minute_interval):
 
         return counts
 
+
 def plot_counts(counts, interval):
 
     plt.figure(figsize=(12, 9), dpi=80)
 
-    # def plot_counts(counts):
     plt.plot(range(0, len(counts)*interval, interval), counts, label='Counts')
-    # print(indices, " ", dialog_per_minutes)
-    # if cnt > 0:
-    #     print(sum/cnt)
     plt.interactive(False)
 
     plt.xlabel('Minutes')
@@ -51,6 +68,26 @@ def plot_counts(counts, interval):
     plt.savefig('%s.png' % movie_name)
     print(counts)
 
-interval = 5
-counts = count_intervals(subs, interval)
-plot_counts(counts, interval)
+
+def plot_counts_percentage(counts):
+
+    plt.figure(figsize=(12, 9), dpi=80)
+
+    plt.plot(range(1, 101), counts, label='Counts')
+    plt.interactive(False)
+
+    plt.xlabel('Percentage')
+    plt.ylabel('Dialog Count')
+    # plt.legend(loc='upper right', numpoints = 1)
+    plt.title("Dialog Tension of %s" % movie_name)
+    # plt.show(block=True)
+    plt.savefig('PERC_%s.png' % movie_name)
+    print(counts)
+
+
+# interval = 1
+# counts = count_intervals(subs, interval)
+# plot_counts(counts, interval)
+# counts_perc = count_percentage(subs)
+# plot_counts_percentage(counts_perc)
+# plot_counts_perc(counts_perc)
--- tension-measuring/eval_subtitles.py
+++ tension-measuring/eval_subtitles.py
@@ -1,12 +1,13 @@
 from collections import namedtuple
 from itertools import groupby
 
+
 def parse_subtitle(filename):
     # "chunk" our input file, delimited by blank lines
     with open(filename, 'rb') as f:
         res = [list(g) for b,g in groupby(f, lambda x: bool(x.strip())) if b]
 
-    Subtitle = namedtuple('Subtitle', 'number start end content at_minute')
+    Subtitle = namedtuple('Subtitle', 'number start end content at_minute at_seconds')
 
     subs = []
 
@@ -18,9 +19,10 @@ def parse_subtitle(filename):
             start_end = sub[1].decode("UTF-8")
             content = sub[2]
 
-            start, end = start_end.split(' --> ')
+            start, end = start_end.split(' --> ') # e.g. 02:14:53,085
             at_minute = int(start[:2]) * 60 + int(start[3:5])
-            subs.append(Subtitle(number, start, end, content, at_minute))
+            at_seconds = int(start[:2]) * 3600 + int(start[3:5]) * 60 + int(start[6:8])
+            subs.append(Subtitle(number, start, end, content, at_minute, at_seconds))
 
     return subs
 
